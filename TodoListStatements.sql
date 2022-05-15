c5c25a82f5a96a757172179894159fb2d066963f
silmu <svetlana.raitina@gmail.com>
2022-05-15 11:21:19 +0300

    Update Route app_homepage to get all todos

---
+++ TodoListStatements.sql
@@ -0,0 +1,5 @@
+use ToDOdb;
+
+insert into todo_list values (1, 'This is a first task', null);
+insert into todo_list values (2, 'This is a second task', null);
+insert into todo_list values (3, 'This is a third task', null);
--- src/Controller/CRUDController.php
+++ src/Controller/CRUDController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Entity\TodoList;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class CRUDController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
-    {
+    public function index(EntityManagerInterface $em): Response
+    {   
+        $tasks = $em->getRepository(TodoList::class)->findBy([], ['id' => 'DESC']);
         return $this->render('crud/index.html.twig', [
-            'controller_name' => 'CRUDController',
+            'tasks' => $tasks
         ]);
     }
 }
