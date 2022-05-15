4082784e93f6d1a667b64508700f123a33905996
LinhLe2307 <lemylinh23071997@gmail.com>
2022-05-15 13:49:01 +0300

    Add create task

--- src/Controller/CRUDController.php
+++ src/Controller/CRUDController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 
 use App\Entity\TodoList;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,5 +20,21 @@ class CRUDController extends AbstractController
         return $this->render('crud/index.html.twig', [
             'tasks' => $tasks
         ]);
+    }
+
+    #[Route('/create', name: 'create_task', methods: ['POST'])]
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $title = trim($request->get('title'));
+        if(!empty($title)) {
+            $entityManager = $doctrine->getManager();
+            $todo = new TodoList();
+            $todo->setTitle($title);
+            $entityManager->persist($todo);
+            $entityManager->flush();
+            return new Response("task added");
+        } else {
+            return $this->redirectToRoute('app_homepage');
+        }
     }
 }
